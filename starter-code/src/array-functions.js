1bb2377729d35a7345a551284a088118bc63cf33
Jeremy Beagle <beaglej2012@fau.edu>
2017-02-20 16:51:22 -0500

    Refactoring complete

--- starter-code/src/array-functions.js
+++ starter-code/src/array-functions.js
@@ -1,10 +1,10 @@
-var ArrayFunctions = function (){}
+class ArrayFunctions {}
 
 // 1) Define a function that takes an array with numbers and prints all the elements of the array, separated by " --- "
 // ArrayFunctions.printSpecial([12, 33, 144, 122])
 // 12 -- 33 -- 144 -- 122
 
-ArrayFunctions.printSpecial = function (array) {
+ArrayFunctions.printSpecial = (array)=> {
   return array.join(" --- ");
 };
 
@@ -12,7 +12,7 @@ ArrayFunctions.printSpecial = function (array) {
 // ArrayFunctions.doubleMyArray([10, 20, 35, 12])
 // [20, 40, 70, 24]
 
-ArrayFunctions.doubleArray = function(array){
+ArrayFunctions.doubleArray = (array)=>{
   return array.map(function(number){
     return number * 2;
   });
@@ -24,10 +24,10 @@ ArrayFunctions.doubleArray = function(array){
 // explanation: (1 x 10^0) + (2 x 10^1) + (3 x 10^2) + (4 x 10^3) + (5 x 10^4)
 // explanation: (1)        + (20)       + (300)      + (4000)     + (50000)
 
-ArrayFunctions.superPower = function(array){
-  return array.reduce(function(sum, number, index){
+ArrayFunctions.superPower = (array)=>{
+  return array.reduce((sum, number, index)=>{
     return sum + (number * (Math.pow(10, index)));
   });
-}
+};
 
 module.exports = ArrayFunctions;
