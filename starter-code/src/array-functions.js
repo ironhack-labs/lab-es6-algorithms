b8060b0b4519b40c63c47155983827aaf887113e
Alice Guan <aliguan726@gmail.com>
2017-04-08 10:13:24 -0400

    update arrayfunctions.js

--- starter-code/src/array-functions.js
+++ starter-code/src/array-functions.js
@@ -1,22 +1,13 @@
 
 
-class Arrays {
-    construtor(array) {
-        this.array = array;
-    }
-    printSpecial() {
-        return this.array.join(" --- ");
-    }
-}
-
-var ArrayFunctions = new Arrays();
+var ArrayFunctions = function() {};
 
 // 1) Define a function that takes an array with numbers and prints all the elements of the array, separated by " --- "
 // ArrayFunctions.printSpecial([12, 33, 144, 122])
 // 12 -- 33 -- 144 -- 122
 
 ArrayFunctions.printSpecial = function (array) {
-  return array.join(" --- ");
+  return array => join(" --- ");
 };
 
 // 2) Define a function that takes an array with numbers and returns another array where each element contains double each element in the array
@@ -24,7 +15,7 @@ ArrayFunctions.printSpecial = function (array) {
 // [20, 40, 70, 24]
 
 ArrayFunctions.doubleArray = function(array){
-  return array.map(number => {
+  return array => map(number => {
     return number * 2;
   });
 };
@@ -36,7 +27,7 @@ ArrayFunctions.doubleArray = function(array){
 // explanation: (1)        + (20)       + (300)      + (4000)     + (50000)
 
 ArrayFunctions.superPower = function(array){
-  return array.reduce((sum, number, index) => {
+  return array => reduce((sum, number, index) => {
     return sum + (number * (Math.pow(10, index)));
   });
 };
