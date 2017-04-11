6b9f59e87aea34bb7b1eb93766390a6c78f417f3
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-04-11 15:16:01 -0400

    done

--- starter-code/src/merge-sort.js
+++ starter-code/src/merge-sort.js
@@ -1,15 +1,15 @@
 function mergeSort(array) {
   if(array.length < 2) { return array }
 
-  var middle  = Math.floor(array.length / 2);
-  var left    = array.slice(0, middle);
-  var right   = array.slice(middle);
+  const middle  = Math.floor(array.length / 2);
+  const left    = array.slice(0, middle);
+  const right   = array.slice(middle);
 
   return sortHalves(mergeSort(left), mergeSort(right));
 }
 
 function sortHalves(left, right) {
-  var array = [];
+  const array = [];
 
   while(left.length && right.length) {
     if(left[0] < right[0]) {
@@ -24,4 +24,4 @@ function sortHalves(left, right) {
   return array.concat(left.slice()).concat(right.slice());
 }
 
-module.exports = mergeSort;
+export default mergeSort;
