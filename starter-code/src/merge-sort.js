1ae7ef63c5c6895d8b5ca856e931419c8ee0ea77
Dianelis Insua <dianelisinsua@Dianelis-MacBook-Air.local>
2017-04-12 17:28:56 -0400

    done

--- starter-code/src/merge-sort.js
+++ starter-code/src/merge-sort.js
@@ -1,23 +1,16 @@
 function mergeSort(array) {
   if(array.length < 2) { return array }
 
-
-const middle = Math.floor(array.length / 2);
-let left = array.slice(0, middle);
-let right = array.slice(middle);
-
-//   var middle  = Math.floor(array.length / 2);
-//   var left    = array.slice(0, middle);
-//   var right   = array.slice(middle);
+  const middle  = Math.floor(array.length / 2);
+  const left    = array.slice(0, middle);
+  const right   = array.slice(middle);
 
   return sortHalves(mergeSort(left), mergeSort(right));
- }
+}
 
 function sortHalves(left, right) {
+  const array = [];
 
-let array =[];
-  // var array = [];
-  //
   while(left.length && right.length) {
     if(left[0] < right[0]) {
       array.push(left.shift());
@@ -28,8 +21,7 @@ let array =[];
   // array.slice() with no arguments is a trick to make a copy of the array
   // .concat is to smash all of the arrays together
   // ...maybe there's an ES6 way to do this?
-  // return array.concat(left.slice()).concat(right.slice());
-  return array.concar(...left, ...right);
+  return array.concat(...left, ...right);
 }
 
 module.exports = mergeSort;
