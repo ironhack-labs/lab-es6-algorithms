4fc42376de5e921f723581174a66f130bc6a3d21
colbyallenc <colbyallenc@gmail.com>
2017-04-14 15:10:48 -0400

    better understanding

--- starter-code/src/bubble-sort.js
+++ starter-code/src/bubble-sort.js
@@ -1,21 +1,27 @@
-
-let array = [12, 15, 0, 3, 6, 5, 10, 7, 9, 3];
-
 function bubbleSort(array) {
-    var length = array.length;
-    for (var i = (length - 1); i > 0; i--) {
+    const length = array.length;
+    //nothing reassigning the length
+    for (let i = (length - 1); i > 0; i--) {
+
+      // i++ is the same as reassigning i because i += 1 and i = i + 1 < is a reassignment
+      //cannot be a const
+
         // Number of passes
-        for (var j = (length - i); j > 0; j--) {
+        for (let j = (length - i); j > 0; j--) {
             // Compare the adjacent positions
             if (array[j] < array[j - 1]) {
                 // Swap the numbers
-                var tmp = array[j];
+                const tmp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = tmp;
             }
+            //can be a const because there is nothing changing tmp
         }
     }
     return array;
 }
 
 module.exports = bubbleSort;
+
+// let and const are block scoped so the scope of them are within the curly races
+//people assume that it works within the entire scope of the function/ the scope of the variable behaved in the way that people expect
