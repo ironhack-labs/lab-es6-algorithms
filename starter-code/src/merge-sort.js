60e3d796e2ff1516073f359add808c94bbb3053a
Carlos Luis <clburguete@gmail.com>
2017-04-24 16:12:44 +0200

    done

--- starter-code/src/merge-sort.js
+++ starter-code/src/merge-sort.js
@@ -1,16 +1,15 @@
 function mergeSort(array) {
   if(array.length < 2) { return array }
 
-  var middle  = Math.floor(array.length / 2);
-  var left    = array.slice(0, middle);
-  var right   = array.slice(middle);
+  const middle  = Math.floor(array.length / 2);
+  let left    = array.slice(0, middle);
+  let right   = array.slice(middle);
 
   return sortHalves(mergeSort(left), mergeSort(right));
 }
 
 function sortHalves(left, right) {
-  var array = [];
-
+  let array = [];
   while(left.length && right.length) {
     if(left[0] < right[0]) {
       array.push(left.shift());
@@ -21,7 +20,12 @@ function sortHalves(left, right) {
   // array.slice() with no arguments is a trick to make a copy of the array
   // .concat is to smash all of the arrays together
   // ...maybe there's an ES6 way to do this?
-  return array.concat(left.slice()).concat(right.slice());
+  //let array2 = array.concat(left.slice()).concat(right.slice());
+  // return array.concat(left.slice()).concat(right.slice());
+  //arr1 = [...arr1, ...arr2];
+   array = [...array, ...left, ...right];
+  //  console.log("left: " + left);
+   return array;
 }
-
+console.log(mergeSort([3,9, 0, 1, 2, 3, 4]));
 module.exports = mergeSort;
