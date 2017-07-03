05815a6c40939f47de689e2a49f1fee5ea71f785
Alex Rodriguez Cid <alexrcid@hotmail.com>
2017-07-03 18:14:08 +0200

    Add files via upload

---
+++ starter-code/src/src/bubble-sort.js
@@ -0,0 +1,18 @@
+function bubbleSort(array) {
+    let length = array.length;
+    for (let i = (length - 1); i > 0; i--) {
+        // Number of passes
+        for (let  j = (length - i); j > 0; j--) {
+            // Compare the adjacent positions
+            if (array[j] < array[j - 1]) {
+                // Swap the numbers
+                let tmp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = tmp;
+            }
+        }
+    }
+    return array;
+}
+
+module.exports = bubbleSort;
