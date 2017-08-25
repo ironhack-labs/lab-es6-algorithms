b604940deca7e2a4b4a79fb8eeca420f12994dca
Nick Navarro <jacinto802@gmail.com>
2017-08-24 22:41:50 -0400

    not done

--- starter-code/src/array-functions.js
+++ starter-code/src/array-functions.js
@@ -4,18 +4,30 @@ var ArrayFunctions = function (){}
 // ArrayFunctions.printSpecial([12, 33, 144, 122])
 // 12 -- 33 -- 144 -- 122
 
+// ArrayFunctions.prototype.printSpecial = function (array) {
+//   return array.join(" --- ");
+// };
+
 ArrayFunctions.prototype.printSpecial = function (array) {
-  return array.join(" --- ");
+  return array.join(" --- ")
 };
+
+
 
 // 2) Define a function that takes an array with numbers and returns another array where each element contains double each element in the array
 // ArrayFunctions.doubleMyArray([10, 20, 35, 12])
 // [20, 40, 70, 24]
 
-ArrayFunctions.prototype.doubleArray = function(array){
-  return array.map(function(number){
-    return number * 2;
-  });
+// ArrayFunctions.prototype.doubleArray = function(array){
+//   return array.map(function(number){
+//     return number * 2;
+//   });
+// };
+
+ArrayFunctions.prototype.doubleArray = function (array) {
+  return array.map(function(x){
+    return x * 2;
+  })
 };
 
 // 3) Define a function that takes an array with numbers and returns the result of multiplying each element by ten to the power of the position it's in:
@@ -23,11 +35,18 @@ ArrayFunctions.prototype.doubleArray = function(array){
 // 54321
 // explanation: (1 x 10^0) + (2 x 10^1) + (3 x 10^2) + (4 x 10^3) + (5 x 10^4)
 // explanation: (1)        + (20)       + (300)      + (4000)     + (50000)
+//
+// ArrayFunctions.prototype.superPower = function(array){
+//   return array.reduce(function(sum, number, index){
+//     return sum + (number * (Math.pow(10, index)));
+//   });
+// }
 
-ArrayFunctions.prototype.superPower = function(array){
+ArrayFunctions.prototype.superPower = function (array) {
   return array.reduce(function(sum, number, index){
     return sum + (number * (Math.pow(10, index)));
-  });
-}
+  })
+};
+
 
 module.exports = ArrayFunctions;
--- starter-code/src/merge-sort.js
+++ starter-code/src/merge-sort.js
@@ -1,15 +1,17 @@
 function mergeSort(array) {
   if(array.length < 2) { return array }
 
-  var middle  = Math.floor(array.length / 2);
-  var left    = array.slice(0, middle);
-  var right   = array.slice(middle);
-
+  let middle  = Math.floor(array.length / 2);
+  console.log("middle= "+middle)
+  let left    = array.slice(0, middle);
+  console.log("left= "+left)
+  let right   = array.slice(middle);
+  console.log("right= "+right)
   return sortHalves(mergeSort(left), mergeSort(right));
 }
 
 function sortHalves(left, right) {
-  var array = [];
+  let array = [];
 
   while(left.length && right.length) {
     if(left[0] < right[0]) {
@@ -18,10 +20,12 @@ function sortHalves(left, right) {
       array.push(right.shift());
     }
   }
-  // array.slice() with no arguments is a trick to make a copy of the array
-  // .concat is to smash all of the arrays together
-  // ...maybe there's an ES6 way to do this?
-  return array.concat(left.slice()).concat(right.slice());
+//   // array.slice() with no arguments is a trick to make a copy of the array
+//   // .concat is to smash all of the arrays together
+//   // ...maybe there's an ES6 way to do this?
+  // return array.concat(left.slice()).concat(right.slice());
+  return array = [...left.map,...right.map];
+//
 }
-
-module.exports = mergeSort;
+//
+// module.exports = mergeSort;
