902bb496ef1d60c6d7f5e8c784c717d5d4a53ce7
Manuel del Pino Lucena <manueldelpinolucena@gmail.com>
2017-09-04 16:00:35 +0200

    2

--- starter-code/src/array-functions.js
+++ starter-code/src/array-functions.js
@@ -15,11 +15,6 @@ class ArrayFunctions{
  const arrayFunctionsObject = new ArrayFunctions();
  arrayFunctionsObject.printSpecial([12, 33, 144, 122]);
  arrayFunctionsObject.doubleArray([10, 20, 35, 12]);
-
-// 3) Define a function that takes an array with numbers and returns the result of multiplying each element by ten to the power of the position it's in:
-// ArrayFunctions.superPower([1,2,3,4,5])
-// 54321
-// explanation: (1 x 10^0) + (2 x 10^1) + (3 x 10^2) + (4 x 10^3) + (5 x 10^4)
-// explanation: (1)        + (20)       + (300)      + (4000)     + (50000)
+ arrayFunctionsObject.superPower([1,2,3,4,5]);
 
 module.exports = ArrayFunctions;
