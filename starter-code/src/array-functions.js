053426e69e55d38f0764366ccba878763cf8e160
Reshma Patel <reshma1529@gmail.com>
2017-09-25 17:52:02 +0200

    Not finish yet

--- starter-code/src/array-functions.js
+++ starter-code/src/array-functions.js
@@ -1,22 +1,35 @@
-var ArrayFunctions = function (){}
+var ArrayFunctions = function (){
 
+}
+class ArrayFunction {
+
+
+
+printSpecial(array){
+  return array.join(" --- ")
+}
+
+
+  doubleMyArray(array){
+    return array.map(function(number)){
+      return number * 2;
+  }
+}
 // 1) Define a function that takes an array with numbers and prints all the elements of the array, separated by " --- "
 // ArrayFunctions.printSpecial([12, 33, 144, 122])
 // 12 -- 33 -- 144 -- 122
 
-ArrayFunctions.prototype.printSpecial = function (array) {
-  return array.join(" --- ");
-};
+
 
 // 2) Define a function that takes an array with numbers and returns another array where each element contains double each element in the array
 // ArrayFunctions.doubleMyArray([10, 20, 35, 12])
 // [20, 40, 70, 24]
 
-ArrayFunctions.prototype.doubleArray = function(array){
-  return array.map(function(number){
-    return number * 2;
-  });
-};
+// // ArrayFunctions.prototype.doubleArray = function(array){
+// //   return array.map(function(number){
+// //     return number * 2;
+// //   });
+// };
 
 // 3) Define a function that takes an array with numbers and returns the result of multiplying each element by ten to the power of the position it's in:
 // ArrayFunctions.superPower([1,2,3,4,5])
