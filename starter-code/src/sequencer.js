9019a05ed61d4719854e03baa313724351e2cd13
Astrid Varga <astrid.varga@gmx.de>
2018-08-20 15:39:18 +0200

    BER Astrid Varga

--- starter-code/src/sequencer.js
+++ starter-code/src/sequencer.js
@@ -1,68 +1,65 @@
-var LetterSequence = function(){}
+class LetterSequence {
+  createSequence(sequence) {
+      const characters = sequence.split("");
+      let containerString = "";
+      let repeatCount = 1;
 
-LetterSequence.prototype.createSequence = function(sequence){
-  var characters      = sequence.split("");
-  var containerString = "";
-  var repeatCount     = 1;
+      characters.forEach((element, index) => {
+          let currentChar = characters[index];
+          let prevChar = characters[index - 1];
+          let nextChar = characters[index + 1];
 
-  for (var i = 0; i < characters.length; i++){
-    var currentChar = characters[i];
-    var prevChar    = characters[i - 1];
-    var nextChar    = characters[i + 1];
+          if (currentChar === prevChar) {
+              repeatCount++;
+          }
 
-    if (currentChar === prevChar){
-      repeatCount++
-    }
-
-    // If the sequence is broken, and the repeat count is greater than 1
-    // add the letter and the repeat count to the return string
-    if (currentChar !== nextChar && repeatCount >= 1){
-      var repeats = repeatCount > 1 ? String(repeatCount) : ""
-      containerString += (repeats + currentChar)
-      repeatCount = 1;
-    }
+          // If the sequence is broken, and the repeat count is greater than 1
+          // add the letter and the repeat count to the return string
+          if (currentChar !== nextChar && repeatCount >= 1) {
+              let repeats = repeatCount > 1 ? String(repeatCount) : "";
+              containerString += (...repeats, ...currentChar);
+              repeatCount = 1;
+          }
+      });
+      return containerString;
   }
+  decodeSequence(sequence) {
+      let containerString = "";
+      const characters = sequence.split("");
 
-  return containerString;
-}
+      characters.forEach((element, index) => {
+          let current = characters[index];
+          let nextChar = characters[index + 1];
 
-LetterSequence.prototype.decodeSequence = function(sequence){
-  var containerString = "";
-  var characters      = sequence.split("");
-
-  for (var i = 0; i < characters.length; i++){
-    var current         = characters[i];
-    var nextChar        = characters[i + 1];
-
-    // If the current character is not a number, then there must be a letter after it
-    if (!isNaN(characters[i])){
-      // So repeat it n times, and add it to our return value
-      var letters = this._repeat(current, nextChar);
-      containerString += letters;
-    // If the current character is a letter, and the last character is a letter, then
-    // it must be a lone letter
-    } else if (isNaN(characters[i]) && isNaN(characters[i - 1])){
-      containerString += characters[i]
-    }
+          // If the current character is not a number, then there must be a letter after it
+          if (!isNaN(characters[index])) {
+              // So repeat it n times, and add it to our return value
+              let letters = this._repeat(current, nextChar);
+              containerString += letters;
+              // If the current character is a letter, and the last character is a letter, then
+              // it must be a lone letter
+          } else if (isNaN(characters[index]) && isNaN(characters[index - 1])) {
+              containerString += characters[index];
+          }
+      });
+      return containerString;
   }
-
-  return containerString;
 }
 
 // Maybe there's a function to do this in ES6...?
 
-LetterSequence.prototype._repeat = function(count, character){
+LetterSequence.prototype._repeat = function(count, character) {
   var characters = "";
 
-  if (count <= 1){
-    count = 1
+  if (count <= 1) {
+      count = 1;
   }
 
-  for (var i = 0; i < count; i++){
-    characters += character;
+  for (var i = 0; i < count; i++) {
+      characters += character;
   }
 
   return characters;
-}
+};
 
 module.exports = LetterSequence;
