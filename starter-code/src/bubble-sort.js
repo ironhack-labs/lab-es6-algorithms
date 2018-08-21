0db60aad65527f390c8b1f38f94c1e847688a6cd
Andrei Fitai <fitai.andrei@gmail.com>
2018-08-21 22:43:01 +0200

    merge-sort and bubble-sort done

--- starter-code/src/bubble-sort.js
+++ starter-code/src/bubble-sort.js
@@ -15,18 +15,6 @@
 //     return array;
 // }
 
-function bubbleSort(arr) {
-  const len = arr.length;
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        const temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
-      }
-    }
-  }
-  return arr
-}
+function bubbleSort(arr) { return arr.sort((a,b) => a-b)}
 
 module.exports = bubbleSort;
